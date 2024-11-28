e09a557725a25c0c1f754cccb3106e59debe75e1
OleksandrTretiak1 <ssanka467@gmail.com>
2024-11-28 23:08:47 +0200

    Добавьте файлы проекта.

---
+++ Лабораторна робота 3 ООП Завдання 2 (1 семестр)/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторна_робота_3_ООП_Завдання_2__1_семестр_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Лабораторна робота 3 ООП Завдання 2 (1 семестр)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лабораторна_робота_3_ООП_Завдання_2__1_семестр_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
